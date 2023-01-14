95bf8bbac6270c27cb494ee452eca64e992a2d36
John Solomou <solomoufam@yahoo.com>
2023-01-14 08:11:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+const cors = require("cors");
+app.use(
+  cors({
+    //   allowedHeaders: ["sessionId", "Content-Type"],
+    //   exposedHeaders: ["sessionId"],
+    "Access-Control-Allow-Origin": "*",
+    //   contentType: "application/json",
+    //   accept: "application/json",
+    //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    //   preflightContinue: false,
+  })
+);
+dotenv.config();
+app.use(express.json());
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8800, () => {
+  console.log("backend server is running on port 8800");
+});
---
+++ routes/movies.js
@@ -0,0 +1,100 @@
+const router = require("express").Router();
+const Movie = require("../model/Movie");
+const verify = require("../verifyToken");
+
+//create
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("not allowed");
+  }
+});
+//update
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updateMovie = await Movie.findByIdAndUpdate(
+        req.body.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateMovie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("not allowed");
+  }
+});
+
+//delete
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.body.id);
+      res.status(200).json("the movie has been deleted");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("not allowed");
+  }
+});
+
+//Get
+router.get("/find/:id", verify, async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Get random movie
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+
+  try {
+    if (type === "series") {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get all
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("not allowed");
+  }
+});
+
+module.exports = router;
